e39cc0797c91189c7057c99a07bacd99f6a932af
ndyt <ntomlinson@gmail.com>
2021-09-17 23:21:44 -0500

    added test to check is_class_full method returns False when class size is empty

--- test_studentlists.py
+++ test_studentlists.py
@@ -134,15 +134,18 @@ class TestStudentLists(TestCase):
     ## TODO write a test for your new is_class_full method when the class is full. 
     # use assertTrue.
     
-    def test_class_size_full_and_equals_max_students(self):
+    def test_class_size_full_and_equals_max_students_returns_true(self):
         test_class = ClassList(2)
         test_class.add_student('Test Student 1')
         test_class.add_student('Test Student 2')
         self.assertTrue(test_class.is_class_full(max_students=2))
 
-
     ## TODO write a test for your new is_class_full method for when is empty, 
     # and when it is not full. Use assertFalse.
+
+    def test_class_size_not_full_and_does_not_match_max_students_returns_false(self):
+        test_class = ClassList(2)
+        self.assertFalse(test_class.is_class_full(max_students=2))
 
 if __name__ == '__main__':
     unittest.main()
