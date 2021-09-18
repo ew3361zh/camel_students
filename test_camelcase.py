6d3e2c82247e5355057651b410272e54ad5f23c5
ndyt <ntomlinson@gmail.com>
2021-09-18 16:55:13 -0500

    spacing changes, added test for single word camelcase

--- test_camelcase.py
+++ test_camelcase.py
@@ -4,17 +4,24 @@ import unittest
 
 class TestCamelCase(unittest.TestCase):
 
+    def test_camelcase_single_word(self):
+        
+        self.assertEqual('flumpy', camelcase_wk4.camel_case_it('FLUmPY'))
+
     def test_camelcase_multiple_words(self):
 
         self.assertEqual('helloWorld', camelcase_wk4.camel_case_it('Hello World'))
     
     def test_camelcase_multiple_spaces(self):
+
         self.assertEqual('', camelcase_wk4.camel_case_it('      '))
     
     def test_camelcase_multiple_words_multiple_spaces(self):
+
         self.assertEqual('helloWorld', camelcase_wk4.camel_case_it('     HELLO          WorlD    '))
     
     def test_camelcase_emojis(self):
+
         self.assertEqual('💃👯‍♀️👙', camelcase_wk4.camel_case_it('💃 👯‍♀️ 👙'))
     
     def test_camelcase_blank(self):
